6c11ba6b4dd3be1b36cd091865245f6086838c94
Hanan Gitliz <hanang@tikalk.com>
2015-10-28 19:43:39 +0200

    added remark

--- src/com/java/course/LearntouseeclipseServlet.java
+++ src/com/java/course/LearntouseeclipseServlet.java
@@ -7,5 +7,5 @@ public class LearntouseeclipseServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
-	}
+	}//
 }
